3b7b38a7ef518eb202dfab01e4d8383cc40f79f6
PuFGGs <35533294+PuFGGs@users.noreply.github.com>
2021-09-21 00:17:39 +0100

    Initial commit

---
+++ Singleton Design Pattern Example/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton_Design_Pattern_Example
+{
+	class Program
+	{
+		private static Singleton _singleton;
+		static void Main(string[] args)
+		{
+			_singleton = Singleton.GetObject();
+
+			int mycalculation = _singleton.AdditionMethod(10, 52);
+
+			_singleton.SendLog(mycalculation.ToString());
+
+			Console.ReadLine();
+		}
+	}
+}
---
+++ Singleton Design Pattern Example/Singleton.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton_Design_Pattern_Example
+{
+	class Singleton
+	{
+		private Singleton() {}
+
+		private static Singleton _object;
+
+		public static Singleton GetObject()
+		{
+			if (_object == null)
+				_object = new Singleton();
+
+			return _object;
+		}
+
+		public void SendLog(string msg)
+		{
+			Console.WriteLine($"Singleton.SendLog(message: \"{msg}\")");
+		}
+
+		public int AdditionMethod(int n1, int n2)
+		{
+			return n2 + n2;
+		}
+	}
+}
